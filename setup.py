431dd59cf93e90398eea3222deb83cbd67ba5169
Vladimir Ulogov <ulogov@adobe.com>
2017-04-02 23:07:57 -0400

    Pre-release commit

--- setup.py
+++ setup.py
@@ -8,5 +8,16 @@ setup(
     license='GPL-3.0',
     author='Vladimir Ulogov',
     author_email='vladimir.ulogov@me.com',
-    description='Slightly naive variable size block cipher'
+    description='Slightly naive variable size block cipher',
+    requires=[
+        "quantumrandom",
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Topic :: Security :: Cryptography',
+    ]
 )
